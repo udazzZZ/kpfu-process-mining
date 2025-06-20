94f5248c760f4107f9dec25002d186f5bfde33b6
Maxim Yurkov <pervostok1999@gmail.com>
2025-06-20 17:24:25 +0400

    feat: processMap

---
+++ src/pages/processMap/ProcessMap.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./ProcessMap.module.css";
+import { Graph } from "widgets/graph";
+
+const ProcessMap: React.FC = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.tabs}>
+                <button className={styles.tabActive}>Карта</button>
+                <button className={styles.tab}>Статистика</button>
+            </div>
+            <div className={styles.graphArea}>
+                <Graph />
+            </div>
+        </div>
+    );
+};
+
+export default ProcessMap;
