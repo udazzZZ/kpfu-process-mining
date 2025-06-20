367b0337a46c717a486cda78c0fccb1b46838a23
udazzZZ <pirogova.dv96@mail.ru>
2025-06-20 16:33:09 +0300

    chore: pull updates

--- src/pages/processMap/ProcessMap.tsx
+++ src/pages/processMap/ProcessMap.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./ProcessMap.module.css";
-import { Graph } from "widgets/graph";
+import { Graph } from "../../widgets/ui/graph/Graph";
 
 const ProcessMap: React.FC = () => {
     return (
